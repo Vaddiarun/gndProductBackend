70d5ee116281e7c0830f3f8d8e25cd57ce929bbb
Vaddiarun <116086928+Vaddiarun@users.noreply.github.com>
2025-09-08 17:00:27 +0530

    Delete src directory

--- src/utils/validateEnv.js
+++
@@ -1,7 +0,0 @@
-export function validateEnv() {
-  const required = ["MONGODB_URI", "JWT_SECRET"];
-  const missing = required.filter((k) => !process.env[k]);
-  if (missing.length) {
-    throw new Error(`Missing environment variables: ${missing.join(", ")}`);
-  }
-}
